083043817286581f32a6851bc6051c905e414928
Sheik Maideen <shefath@hotmail.com>
2020-04-05 12:58:56 +0530

    Oshop Created with Firebase and Bootstrap step1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+//Fire base realted modules
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { environment } from "src/environments/environment";
+
+import { AppComponent } from "./app.component";
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BsNavbarComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDvyhQeQsdPECDPS51U3-pa5fi40hdgpHw",
+    authDomain: "oshop-47e63.firebaseapp.com",
+    databaseURL: "https://oshop-47e63.firebaseio.com",
+    projectId: "oshop-47e63",
+    storageBucket: "oshop-47e63.appspot.com",
+    messagingSenderId: "925626676525",
+    appId: "1:925626676525:web:fe09647e8d179176874fa4"
+  }
 };
